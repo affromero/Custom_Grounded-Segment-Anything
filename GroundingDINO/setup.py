d1ab9acc70b44de609b2806ed34250101b984619
Andrés Romero <affromero@users.noreply.github.com>
2024-07-10 11:38:50 -0500

    Update setup.py

--- GroundingDINO/setup.py
+++ GroundingDINO/setup.py
@@ -29,6 +29,7 @@ try:
     import torch
 except ImportError:
     subprocess.check_call([sys.executable, "-m", "pip", "install", "torch @ https://download.pytorch.org/whl/cu121/torch-2.1.2%2Bcu121-cp310-cp310-linux_x86_64.whl"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "numpy", "--upgrade"])
     import torch
 from setuptools import find_packages, setup
 from torch.utils.cpp_extension import CUDA_HOME, CppExtension, CUDAExtension
