b7134f8895b20ada900098ee16e289c5efe6f5cf
Andrés Romero <affromero@users.noreply.github.com>
2024-07-10 14:17:17 -0500

    Update and rename _setup.py to setup.py
    remove cuda dependencies for poetry. too many conflict

--- GroundingDINO/setup.py
+++ GroundingDINO/setup.py
@@ -25,17 +25,7 @@ import os
 import subprocess
 import sys
 
-try:
-    import torch
-except ImportError:
-    subprocess.check_call([sys.executable, "-m", "ensurepip"])
-    # subprocess.check_call([sys.executable, "-m", "pip", "install", "setuptools<70"]) # ImportError: cannot import name 'packaging' from 'pkg_resources'
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "setuptools==69.5.1", "numpy<2", "torch @ https://download.pytorch.org/whl/cu121/torch-2.1.2%2Bcu121-cp310-cp310-linux_x86_64.whl"])
-    # subprocess.check_call([sys.executable, "-m", "pip", "install", "numpy", "--upgrade"])
-    # subprocess.check_call([sys.executable, "-m", "pip", "install", "numpy<2"])
-    import torch
 from setuptools import find_packages, setup
-from torch.utils.cpp_extension import CUDA_HOME, CppExtension, CUDAExtension
 
 # groundingdino version info
 version = "0.1.0"
@@ -55,69 +45,6 @@ def write_version_file():
     with open(version_path, "w") as f:
         f.write(f"__version__ = '{version}'\n")
         # f.write(f"git_version = {repr(sha)}\n")
-
-
-requirements = ["torch", "torchvision"]
-
-torch_ver = [int(x) for x in torch.__version__.split(".")[:2]]
-
-
-def get_extensions():
-    this_dir = os.path.dirname(os.path.abspath(__file__))
-    extensions_dir = os.path.join(this_dir, "groundingdino", "models", "GroundingDINO", "csrc")
-
-    main_source = os.path.join(extensions_dir, "vision.cpp")
-    sources = glob.glob(os.path.join(extensions_dir, "**", "*.cpp"))
-    source_cuda = glob.glob(os.path.join(extensions_dir, "**", "*.cu")) + glob.glob(
-        os.path.join(extensions_dir, "*.cu")
-    )
-
-    sources = [main_source] + sources
-
-    # We need these variables to build with CUDA when we create the Docker image
-    # It solves https://github.com/IDEA-Research/Grounded-Segment-Anything/issues/53
-    # and https://github.com/IDEA-Research/Grounded-Segment-Anything/issues/84 when running
-    # inside a Docker container.
-    am_i_docker = os.environ.get('AM_I_DOCKER', '').casefold() in ['true', '1', 't']
-    use_cuda = os.environ.get('BUILD_WITH_CUDA', '').casefold() in ['true', '1', 't']
-
-    extension = CppExtension
-
-    extra_compile_args = {"cxx": []}
-    define_macros = []
-
-    if (torch.cuda.is_available() and CUDA_HOME is not None) or \
-            (am_i_docker and use_cuda):
-        print("Compiling with CUDA")
-        extension = CUDAExtension
-        sources += source_cuda
-        define_macros += [("WITH_CUDA", None)]
-        extra_compile_args["nvcc"] = [
-            "-DCUDA_HAS_FP16=1",
-            "-D__CUDA_NO_HALF_OPERATORS__",
-            "-D__CUDA_NO_HALF_CONVERSIONS__",
-            "-D__CUDA_NO_HALF2_OPERATORS__",
-        ]
-    else:
-        print("Compiling without CUDA")
-        define_macros += [("WITH_HIP", None)]
-        extra_compile_args["nvcc"] = []
-        return None
-
-    sources = [os.path.join(extensions_dir, s) for s in sources]
-    include_dirs = [extensions_dir]
-
-    ext_modules = [
-        extension(
-            "groundingdino._C",
-            sources,
-            include_dirs=include_dirs,
-            define_macros=define_macros,
-            extra_compile_args=extra_compile_args,
-        )
-    ]
-
-    return ext_modules
 
 
 def parse_requirements(fname="requirements.txt", with_version=True):
@@ -220,6 +147,5 @@ if __name__ == "__main__":
                 "tests",
             )
         ),
-        ext_modules=get_extensions(),
-        cmdclass={"build_ext": torch.utils.cpp_extension.BuildExtension},
+        ext_modules=None,
     )
